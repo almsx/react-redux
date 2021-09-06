c28f0a1221b2683d75ffb4dad029b4735e917422
Alberto Luebbert M <alberto@ideashappy.com>
2021-09-06 07:23:19 -0500

    Index. Add bootstrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+// Bootstrap
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <React.StrictMode>
